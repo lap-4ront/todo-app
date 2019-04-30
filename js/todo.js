1e8b9d7e6cecbfd7b7a67d4d01d8a1d7c5e52a2c
tnt2ultra <46776367+tnt2ultra@users.noreply.github.com>
2019-04-30 14:29:02 +0300

    Add persistence
    Save json data for all tasks

--- js/todo.js
+++ js/todo.js
@@ -1,15 +1,21 @@
+const jsonURL = "https://api.myjson.com/bins/dwbh4";
+
 let tasks = []; // all tasks array: 0 - completed, 1 - active
+let app_name;
+let copyright;
+let version;
+let active_tasks;
+let complete_tasks;
 
 function onLoad() {
-    const jsonURL = "https://api.myjson.com/bins/dwbh4";
     $.getJSON(jsonURL, function (data) {
-        // let app_name = data.app_name;
-        // let copyright = data.copyright;
-        // let version = data.version;
-        let active_tasks = data.active_tasks;
-        let complete_tasks = data.complete_tasks;
-
         let numTask = 1; // Current task number for div id
+
+        app_name = data.app_name;
+        copyright = data.copyright;
+        version = data.version;
+        active_tasks = data.active_tasks;
+        complete_tasks = data.complete_tasks;
 
         // Add active tasks to task list
         active_tasks.forEach(element => {
@@ -24,14 +30,13 @@ function onLoad() {
             let str = setStrDiv(element, numTask, 'strikeout', 'checked');
             tasks[numTask] = 0;
             numTask++;
-            $("#active_tasks").append(str);
+            $("#complete_tasks").append(str);
         });
 
-        addOnClick();
+        addOnClick(); // Add event handler
 
         $(".wait").remove(); // delete animated gif
     });
-
 }
 
 // create html-text for task div
@@ -55,5 +60,56 @@ function addOnClick() {
             $("#id" + id).removeClass("checked");
             $("#ida" + id).removeClass("strikeout");
         }
+        saveJson();
     })
+}
+
+// Save changes
+function saveJson() {
+    let jsonData = createJson();
+    $.ajax({
+        url: jsonURL,
+        type: "PUT",
+        data: jsonData,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+            // Saved
+        }
+    });
+
+}
+
+// Create json data
+function createJson() {
+    let data = "{";
+    
+    data += '"app_name":"' + app_name + '",';
+    data += '"version":' + version + ',';
+    data += '"copyright":"' + copyright + '",';
+    data += '"active_tasks":[';
+
+    let numTask = 1;
+    let strActive = "";
+    let strCompleted = "";
+
+    function setOneTaskData(el) {
+        if (tasks[numTask] === 1) {
+            if (strActive !== "")
+                strActive += ',';
+            strActive += '"' + el + '"';
+        } else {
+            if (strCompleted !== "")
+                strCompleted += ',';
+            strCompleted += '"' + el + '"';
+        }
+        numTask++;
+    }
+
+    active_tasks.forEach(element => setOneTaskData(element));
+    complete_tasks.forEach(element => setOneTaskData(element));
+
+    data += strActive + '],"complete_tasks": [' + strCompleted + ']}';
+
+    return data;
 }
